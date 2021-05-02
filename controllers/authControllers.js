5e02c4e869a93d898d3486c4508027fd50fba38f
Dev-Harmattan <ganiyusodiq059@gmail.com>
2021-05-02 05:17:00 -1200

    implement model validation and handleError method

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,22 @@
 const User = require('../models/user');
 
+const handleErrors = (err) => {
+  // console.log(err.message, err.code);
+  let errors = {email: '', password: ''}
+  
+  if(err.code  === 11000){
+    errors.email = 'User with that email already exist';
+  }
+  
+  if(err.message.includes('user validation failed')){
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
+
 exports.signup_get = (req, res) => {
   res.render('signUp');
 }
@@ -14,8 +31,8 @@ exports.signup_post = async (req, res) => {
     const user = await User.create({email, password});
     res.status(201).json({user});
   } catch (error) {
-    console.log(error)
-    res.status(400).send('Error, user not created!');
+    const errors = handleErrors(error);
+    res.status(400).json({errors});
   }
 }
 
