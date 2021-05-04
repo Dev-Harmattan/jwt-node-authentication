14348839fbdf8328d788e6c62f0fd3eafa114180
Dev-Harmattan <ganiyusodiq059@gmail.com>
2021-05-03 12:50:34 -1200

    setup cookie

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
+app.use(cookieParser());
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -24,4 +26,14 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.get('/set-cookies', (req, res) => {
+  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+  res.send('Cookies set');
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+})
 app.use(authRoutes);
